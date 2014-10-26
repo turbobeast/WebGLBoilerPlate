0ce2f385cf2c191550c1fbd69a97eed163086d5f
george michael faust <georgemichaelfaust@gmail.com>
2014-10-25 23:53:07 -0400

    clean up vert shader

--- src/shaders/vertex.glsl
+++ src/shaders/vertex.glsl
@@ -1,8 +1,5 @@
 attribute vec4 aVertexPosition;
 attribute vec3 aColor;
-uniform mat4 uModelMatrix;
-uniform mat4 uViewMatrix;
-uniform mat4 uProjMatrix;
 varying vec3 vColor;
 
 uniform mat4 uMVPMatrix;
